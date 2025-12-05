b4c29d16122d76ab6216e241c25e15889b089023
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-12-05 21:46:36 +0600

    Exercise 12 to 16 completed

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    total += element;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = 1;
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    total *= element;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  return base ** exponent
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let total = 1;
+  for (let i = 2; i <= n; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
